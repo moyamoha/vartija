97ca4d4dc5002060300880602a5be74c0f8b0fb3
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-05-13 20:50:57 +0300

    change messaging type a little bit

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+  MessageEvent,
+  Sse,
+} from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { AuthTokenGaurd } from 'src/config/auth-token.gaurd';
+import { CustomReq } from 'src/types/custom';
+import { Observable, interval, map } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +18,24 @@ export class AppController {
   @Get('')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthTokenGaurd)
+  @Sse('notifications')
+  sse(@Req() req: CustomReq): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      map((_) => ({
+        data: {
+          isActive: req.user.isActive,
+          profile: {
+            firstName: req.user.firstname,
+            lastName: req.user.lastname,
+            email: req.user.email,
+            mfaEnabled: req.user.mfa.enabled,
+          },
+        },
+      })),
+    );
   }
 }
